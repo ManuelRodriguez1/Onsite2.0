61b8c7919ac17fbd6cd66c1c26787cabe6c81756
Manuel <manuelitoys93@gmail.com>
2018-11-23 09:30:02 -0500

    Creacion carpeta components, services, routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProComponent } from './components/pro/pro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProComponent
   ],
   imports: [
     BrowserModule
