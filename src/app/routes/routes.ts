2ca38622fc18ecf357847d195cb90eeb4c33b7f9
Duvan <duvan@nativo.design>
2018-11-26 08:46:32 -0500

    Auto stash before merge of "master" and "origin/master"
    o

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from "@angular/router";
 import { ProComponent } from "../components/pro/pro.component";
+import { HomeComponent } from "../components/home/home.component";
+
 
 export const router: Routes = [
-    { path: '', redirectTo: 'Home', pathMatch: 'full' },
+    // { path: '', redirectTo: 'Home', pathMatch: 'full' },
+    { path: 'Home', component: HomeComponent },
     { path: 'Pro', component: ProComponent }
 ]
