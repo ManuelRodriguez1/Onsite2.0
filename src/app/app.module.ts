f8a253d7c164ed7fac40c9ebe85f95b5e20e7201
Unknown <katheryn@nativo.design>
2018-12-04 10:35:42 -0500

    hire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProComponent } from './components/pro/pro.component';
+import { HireComponent } from './components/hire/hire.component';
+
 import { MenuComponent } from './components/menu/menu.component';
 
 import { router } from "./routes/routes";
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
+
+import * as firebase from 'firebase/app';
+import { environment } from '../environments/environment';
+
 
 
 
@@ -16,7 +22,8 @@ import { HomeComponent } from "./components/home/home.component";
     AppComponent,
     ProComponent,
     MenuComponent,
-    HomeComponent
+    HomeComponent,
+    HireComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,18 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
-};
+  production: true,
 
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  // Initialize Firebase
+/*  firebase: {
+    apiKey: "AIzaSyACtHB8NrW_kXD2oX1DZ3l7bvUrvJCJXQk",
+    authDomain: "onsite-92699.firebaseapp.com",
+    databaseURL: "https://onsite-92699.firebaseio.com",
+    projectId: "onsite-92699",
+    storageBucket: "onsite-92699.appspot.com",
+    messagingSenderId: "1041762411923"
+  }*/
+};
