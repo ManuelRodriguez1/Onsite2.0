f79e76f3d4a96547ddaba5d61cbb2cb14fc6ccda
Manuel <manuelitoys93@gmail.com>
2018-12-13 15:57:10 -0500

    test

--- src/app/components/pro/profile-pro/explore/explore.component.ts
+++ src/app/components/pro/profile-pro/explore/explore.component.ts
@@ -10,9 +10,13 @@ export class ExploreComponent implements OnInit {
   skills = ['Concrete', 'Decorator', 'Drywall', 'Electrical', 'Excavation', 'Flooring', 'General Labor', 'Insulation', 'Interior Fishing Carpentry', 'Iron Worker', 'Landscaper', 'Mason', 'Plastering', 'Plumbing', 'Roofer', 'Waterproof Installation'];
   lat: number = 51.678418;
   lng: number = 7.809007;
+  dots = []
   constructor() { }
 
   ngOnInit() {
+    for (let i = 0; i < (this.skills.length / 4); i++) {
+      this.dots.push(i)
+    }
   }
 
 }
