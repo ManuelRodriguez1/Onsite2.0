11f5584ea747c83a40e12160725238f5303cd14d
Unknown <katheryn@nativo.design>
2018-12-13 10:17:52 -0500

    commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, ViewChild,  Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,45 +12,37 @@ export class LoginComponent implements OnInit {
   error: any[];
   password;
   email;
-  cerrarAbrir="";
+  cerrarAbrir = "";
   abrir = false;
-  constructor(public af: AngularFireAuth,private  router: Router) {
+  constructor(public af: AngularFireAuth, private router: Router) {
     this.af.authState.subscribe(authState => {
-      console.log("hola");
       console.log(authState);
-    if(authState) {
-      console.log("11111");
-      this.router.navigateByUrl('/Hireprincipal');
-  }else{
-    console.log("3333");
-  }
-    });
-   }
+      if (authState) {
 
+   firebase.database().ref('/users/belxy1' ).once('value').then(function(snapshot) {
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    console.log(snapshot.val());
+  });
+
+        this.router.navigateByUrl('/Hireprincipal');
+      } else {
+        console.log("3333");
+      }
+    });
+  }
   ngOnInit() {
   }
-
   onSubmit(formData) {
-  if(formData.valid) {
-    this.af.auth.signInWithEmailAndPassword(formData.value.email,formData.value.password).then((resolve)=>{
-      console.log("HOOOOOLLLLLLLSSSSSSSSSSS");
-      //const status ="online";
-  //    this.setUserStatus(formData.value.email,status);
-
-
-formData.reset();
-  this.abrir=true;
-
-
-
-    })
-    .catch(
-      (err) => {
-      this.error = err.message;
-    })
+    if (formData.valid) {
+      this.af.auth.signInWithEmailAndPassword(formData.value.email, formData.value.password).then((resolve) => {
+        console.log("HOOOOOLLLLLLLSSSSSSSSSSS");
+        formData.reset();
+        this.abrir = true;
+      })
+        .catch(
+          (err) => {
+            this.error = err.message;
+          })
+    }
   }
-}
-
-
-
 }
