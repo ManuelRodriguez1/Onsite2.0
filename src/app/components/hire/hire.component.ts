dc4a09f9a5353ff35b826c615ee0276e218ff3d8
Cristian Rodriguez <cristian@nativo.design>
2020-04-24 09:17:09 -0500

    Auto stash before rebase of "origin/master"
    Register user hire

--- src/app/components/hire/hire.component.ts
+++ src/app/components/hire/hire.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase/app';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Router } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase/app";
+import { AngularFireAuth } from "angularfire2/auth";
+import { Router } from "@angular/router";
+import { FormGroup, FormControl } from "@angular/forms";
 import { AngularFirestore } from "angularfire2/firestore";
-import {AppComponent }  from '../../app.component';
+import { AppComponent } from "../../app.component";
 
 @Component({
-  selector: 'app-hire',
-  templateUrl: './hire.component.html',
-  styleUrls: ['./hire.component.css']
+  selector: "app-hire",
+  templateUrl: "./hire.component.html",
+  styleUrls: ["./hire.component.css"],
 })
 export class HireComponent implements OnInit {
   FirstName;
@@ -21,79 +21,66 @@ export class HireComponent implements OnInit {
   error;
   appComponent = AppComponent;
 
-  constructor(public af: AngularFireAuth, private router: Router, private db: AngularFirestore) {
-
-
-  }
-  ngOnInit() {
-    
-
-  }
+  constructor(
+    public af: AngularFireAuth,
+    private router: Router,
+    private db: AngularFirestore
+  ) {}
+  ngOnInit() {}
 
   SendVerificationMail() {
-    return this.af.auth.currentUser.sendEmailVerification()
-    .then(() => {
-      this.router.navigate(['/Hireprincipal']);
-    })
+    return this.af.auth.currentUser.sendEmailVerification().then(() => {
+      this.router.navigate(["/Hireprincipal"]);
+    });
   }
 
-
   onSubmit(formData) {
-
     console.log(formData);
     if (formData.valid) {
-      this.af.auth.createUserWithEmailAndPassword(
-        formData.value.Email,
-        formData.value.Password
-      ).then((result) => {
-        this.SendVerificationMail();
-      }).then(
-        (success) => {
+      this.af.auth
+        .createUserWithEmailAndPassword(
+          formData.value.Email,
+          formData.value.Password
+        )
+        .then((result) => {
+          this.SendVerificationMail();
+        })
+        .then((success) => {
           var user = firebase.auth().currentUser;
           user.updateProfile({
             displayName: "hire",
             photoURL: "",
           });
-          this.db.collection('users_hire').add({
+          this.db.collection("users_hire").add({
             nombre: formData.value.FirstName,
             apellido: formData.value.LastName,
             telefono: formData.value.PhoneNumber,
             correo: user.email,
             zipcode: formData.value.Entercityorzipcode,
-            estado: 'hire'
-          })
-          this.router.navigateByUrl('/Hireprincipal');
-        }).catch(
-          (err) => {
-            this.error = err.message;
-            console.log(err);
-       
-          })
-    }
-    else {
+            estado: "hire",
+          });
+          this.router.navigateByUrl("/Hireprincipal");
+        })
+        .catch((err) => {
+          this.error = err.message;
+          console.log(err);
+        });
+    } else {
       if (formData.control.controls.FirstName.status == "INVALID") {
         this.error = "Require FirstName";
-      }
-      else if (formData.control.controls.LastName.status == "INVALID") {
+      } else if (formData.control.controls.LastName.status == "INVALID") {
         this.error = "Require LastName";
-      }
-      else if (formData.control.controls.PhoneNumber.status == "INVALID") {
+      } else if (formData.control.controls.PhoneNumber.status == "INVALID") {
         this.error = "Require PhoneNumber";
-      }
-      else if (formData.control.controls.Email.status == "INVALID") {
+      } else if (formData.control.controls.Email.status == "INVALID") {
         this.error = "Require email";
-      }
-      else if (formData.control.controls.Password.status == "INVALID") {
+      } else if (formData.control.controls.Password.status == "INVALID") {
         this.error = "Require Password";
-      }
-      else if (formData.control.controls.Entercityorzipcode.status == "INVALID") {
+      } else if (
+        formData.control.controls.Entercityorzipcode.status == "INVALID"
+      ) {
         this.error = "Require Entercityorzipcode";
       }
-
     }
-
-
-
   }
-
 }
