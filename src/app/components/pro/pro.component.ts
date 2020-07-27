33aece360c21a26d01b63c82926b6dddda2afb6c
katheryn <Katheryn@nativo.design>
2020-07-27 12:51:47 -0500

    cambio de diseño skills

--- src/app/components/pro/pro.component.ts
+++ src/app/components/pro/pro.component.ts
@@ -14,11 +14,11 @@ export class ProComponent implements OnInit {
 
   cust = 0;
   select = 0;
-  up = false;
+  up = true;
   up2 = false;
   page = 0;
   selectskills: any[] = [];
-  selectskills2 = null;
+  //selectskills2 = null;
   title = ['Enter your information:', 'Select skills'];
   text = ['About You', 'Your Skills'];
   skills = ['Concrete', 'Decorator', 'Drywall', 'Electrical', 'Excavation', 'Flooring', 'General Labor', 'Insulation', 'Interior Finishing Carpentry', 'Iron Worker', 'Landscaper', 'Mason', 'Plastering', 'Plumbing', 'Roofer', 'Waterproof Installation'];
@@ -71,25 +71,25 @@ export class ProComponent implements OnInit {
       }
     })
 
-    $('html').on('click', () => {
+    /*$('html').on('click', () => {
       this.up = false
     })
 
     $("#clickSkills, .skillselct").click(function (e) {
       e.stopPropagation()
-    })
+    })*/
 
 
   }
 
-
+/*
   list(e: number) {
     if (e == 1) {
       this.up = !this.up;
     } else {
       this.up2 = !this.up2;
     }
-  }
+  }*/
 
   next() {
     this.page++
@@ -101,19 +101,39 @@ export class ProComponent implements OnInit {
   }
   selectskill(e) {
     // this.up = !this.up;
-    var i = this.selectskills.indexOf(e)
-    i === -1 && this.selectskills.push(e);
-    this.skills2 = [this.selectskills + ' Hanger', this.selectskills + ' Apprentice', 'Metal Framer',
-      'Metal Framer Apprentice', this.selectskills + ' Finisher', 'Fire Taper'];
+
+    var i = this.selectskills.indexOf(e);
+    var checkbox = $("input[title='input" + e + "']");
+    checkbox.prop("checked", !checkbox.prop("checked"));
+
+    if ($(checkbox).is(':checked')) {
+      $("#divSkill"+e).addClass("containerSkillNewSelect");
+      var i = this.selectskills.indexOf(e);
+      if (i == -1) {
+        this.selectskills.push(e);
+      }
+    } else {
+      $("#divSkill"+e).removeClass("containerSkillNewSelect");
+      var i = this.selectskills.indexOf(e)
+      i !== -1 && this.selectskills.splice(i, 1)
+    }
+
+
+
+
+
+ 
+    console.log(this.selectskills);
+    //this.skills2 = [this.selectskills + ' Hanger', this.selectskills + ' Apprentice', 'Metal Framer','Metal Framer Apprentice', this.selectskills + ' Finisher', 'Fire Taper'];
   }
   selectskill2(e) {
-    this.up2 = !this.up2;
-    this.selectskills2 = e
+    //this.up2 = !this.up2;
+    //this.selectskills2 = e
   }
   close(e) {
     var i = this.selectskills.indexOf(e)
     i !== -1 && this.selectskills.splice(i, 1)
-    e == 2 && this.selectskills2 == null
+    //e == 2 && this.selectskills2 == null
   }
   addcustomer() {
     this.customers.push('Add certificate file');
